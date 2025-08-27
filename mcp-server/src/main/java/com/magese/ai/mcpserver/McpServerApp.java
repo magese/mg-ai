bffd50c90b842f63f20ca696cf79008fc825a6ef
Magese <magese@live.cn>
2025-08-27 09:30:51 +0800

    init

---
+++ mcp-server/src/main/java/com/magese/ai/mcpserver/McpServerApp.java
@@ -0,0 +1,26 @@
+package com.magese.ai.mcpserver;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * MCP服务端
+ *
+ * @author Magese
+ * @since 2025/8/26 17:20
+ */
+@Slf4j
+@SpringBootApplication
+public class McpServerApp {
+
+    public static void main(String[] args) {
+        try {
+            log.info("Starting MCP Server");
+            SpringApplication.run(McpServerApp.class, args);
+            log.info("MCP Server started");
+        }  catch (Exception e) {
+            log.error("Error starting MCP Server", e);
+        }
+    }
+}
