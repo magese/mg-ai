88d0be95904e12bacc336d1e1cfa6e61d95770a8
Magese <magese@live.cn>
2025-08-29 17:43:15 +0800

    mcp client

---
+++ mcp-client/src/main/java/com/magese/ai/mcpclient/service/DoubaoAssistant.java
@@ -0,0 +1,19 @@
+package com.magese.ai.mcpclient.service;
+
+import dev.langchain4j.service.SystemMessage;
+import dev.langchain4j.service.spring.AiService;
+import dev.langchain4j.service.spring.AiServiceWiringMode;
+import reactor.core.publisher.Flux;
+
+/**
+ * AI助手
+ *
+ * @author Magese
+ * @since 2025/8/29 16:20
+ */
+@AiService(wiringMode = AiServiceWiringMode.EXPLICIT, streamingChatModel = "ep-20250829155452-dp8pl", tools = "calcTool")
+public interface DoubaoAssistant {
+
+    @SystemMessage("你是一个高级智能助手，请用最简短且清晰的答案来回答用户的问题。")
+    Flux<String> chat(String message);
+}
