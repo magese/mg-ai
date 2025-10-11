19653e1576ecb90761784349cf441853fd2b613d
Magese <magese@live.cn>
2025-10-11 10:48:39 +0800

    postgres聊天记忆存储

--- mcp-agent/src/main/java/com/magese/ai/mcpagent/config/ChatClientConfig.java
+++ mcp-agent/src/main/java/com/magese/ai/mcpagent/config/ChatClientConfig.java
@@ -5,7 +5,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
 import org.springframework.ai.chat.memory.ChatMemory;
-import org.springframework.ai.chat.memory.ChatMemoryRepository;
 import org.springframework.ai.chat.memory.MessageWindowChatMemory;
 import org.springframework.ai.chat.memory.repository.jdbc.JdbcChatMemoryRepository;
 import org.springframework.ai.chat.memory.repository.jdbc.PostgresChatMemoryRepositoryDialect;
