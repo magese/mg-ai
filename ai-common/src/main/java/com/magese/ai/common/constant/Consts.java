83e9c1d610eaab242ab43d4373a931996529ba19
Magese <magese@live.cn>
2026-05-13 17:12:02 +0800

    save

---
+++ ai-common/src/main/java/com/magese/ai/common/constant/Consts.java
@@ -0,0 +1,14 @@
+package com.magese.ai.common.constant;
+
+/**
+ * 常量
+ *
+ * @author Magese
+ * @since 2025/10/13 09:27
+ */
+public interface Consts {
+
+    String LOCAL_FILE_DIR = "/app/data/mcp-agent/";
+
+    String USER_ID_FUFU = "fufu20251015";
+}
